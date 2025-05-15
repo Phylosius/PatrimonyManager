4f16c9d3425068fe7d78df632c1979b0711d0f4c
Phylosius <hei.phylosius@gmail.com>
2025-05-15 11:38:58 +0300

    chore: remove LifeTrain implementation

--- src/main/java/school/hei/PatrimonyManager/LifeTrain.java
+++ src/main/java/school/hei/PatrimonyManager/LifeTrain.java
@@ -3,13 +3,7 @@ package school.hei.PatrimonyManager;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.time.Duration;
-
 @EqualsAndHashCode(callSuper = true)
 @Data
 public final class LifeTrain extends Possession{
-    private String name;
-    private String duration;
-    private Money moneySource;
-    private Duration applyEach;
 }
