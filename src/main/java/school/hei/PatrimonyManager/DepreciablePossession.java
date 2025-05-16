404827e89bd1b1d0f1aede9f66cff0d7545c00d6
Phylosius <hei.phylosius@gmail.com>
2025-05-16 06:52:57 +0300

    chore: implement DepreciablePossession

--- src/main/java/school/hei/PatrimonyManager/DepreciablePossession.java
+++ src/main/java/school/hei/PatrimonyManager/DepreciablePossession.java
@@ -3,7 +3,10 @@ package school.hei.PatrimonyManager;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDate;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public sealed class DepreciablePossession extends Possession permits MaterialGood, BankAccount{
+    private LocalDate lastDepreciationDate;
 }
