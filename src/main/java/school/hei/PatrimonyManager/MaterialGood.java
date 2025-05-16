d1029e4201e2bdf3e42cd71544cf21514decb96a
Phylosius <hei.phylosius@gmail.com>
2025-05-16 06:56:03 +0300

    chore: implement MaterialGood

--- src/main/java/school/hei/PatrimonyManager/MaterialGood.java
+++ src/main/java/school/hei/PatrimonyManager/MaterialGood.java
@@ -3,7 +3,10 @@ package school.hei.PatrimonyManager;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDate;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public final class MaterialGood extends DepreciablePossession {
+    private LocalDate acquisitionDate;
 }
