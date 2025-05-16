f6ea7f5d7df2dd89d0c9f5ad309c741ba73715b2
Phylosius <hei.phylosius@gmail.com>
2025-05-16 09:23:41 +0300

    chore: implement LifeTrain

--- src/main/java/school/hei/PatrimonyManager/BankAccount.java
+++ src/main/java/school/hei/PatrimonyManager/BankAccount.java
@@ -1,9 +1,14 @@
 package school.hei.PatrimonyManager;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDate;
+
 @EqualsAndHashCode(callSuper = true)
-@Data
 public final class BankAccount extends DepreciablePossession{
+
+    public BankAccount(String name, Money value,
+                       LocalDate lastDepreciationDate, DepreciationRate depreciationRate) {
+        super(name, value, lastDepreciationDate, depreciationRate);
+    }
 }
--- src/main/java/school/hei/PatrimonyManager/LifeTrain.java
+++ src/main/java/school/hei/PatrimonyManager/LifeTrain.java
@@ -1,9 +1,32 @@
 package school.hei.PatrimonyManager;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.Duration;
+
+@Getter
+@Setter
 @EqualsAndHashCode(callSuper = true)
-@Data
 public final class LifeTrain extends Possession{
+    private BankAccount meansOfIncome;
+    private Duration applyEach;
+
+    public LifeTrain(String name, Money value,
+                     BankAccount meansOfIncome, Duration applyEach) {
+        super(name, value);
+        this.meansOfIncome = meansOfIncome;
+        this.applyEach = applyEach;
+    }
+
+    private Money getTotalValueIn(Duration duration) {
+        double applianceCoefficient = (double) applyEach.dividedBy(duration);
+        Money value = new Money();
+
+        value.setCurrency(value.getCurrency());
+        value.setAmount(value.getAmount() * applianceCoefficient);
+
+        return value;
+    }
 }
